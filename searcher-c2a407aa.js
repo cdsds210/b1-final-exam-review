b42b1f93fdad91df8f678b1edac4f7eb449d7344
JoeyRussoniello <JoeyRussoniello@users.noreply.github.com>
2025-12-09 14:25:13 +0000

    deploy: 985ee233c994acdb7fec97ac09562c55dd6a7294

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-eaf6e403.js',
+                path_to_root + 'searchindex-1266cf32.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
